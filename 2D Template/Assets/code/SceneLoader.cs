7ab5ada786f15fb9b99dafbffbbaf935324887bf
MUKILTEO\1722815 <1722815@mukilteo.wednet.edu>
2024-12-19 11:16:23 -0800

    bug

--- 2D Template/Assets/code/SceneLoader.cs
+++ 2D Template/Assets/code/SceneLoader.cs
@@ -7,11 +7,9 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
     public string sceneName;
-<<<<<<< HEAD
-    public bool canChangeScenes;
-=======
+
     public bool canChangeScenes = true;
->>>>>>> 0a2b88fc8d4593096f4c1e338fea74976252634f
+
     public float waitTimeSeconds;
     private GameObject Door;
     
